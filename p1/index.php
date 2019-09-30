c2029c3e3d080d3bc3a4c756f5a9d8e930571cd8
RBiss <55113088+RBiss@users.noreply.github.com>
2019-09-30 16:28:20 -0700

    Add files via upload
    Week 4 Lab

---
+++ p1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+for ($i = 7; $i <= 17; $i++) {
+    // echo "<img src=\"images/$i.png\" alt=\"$i\">";
+    ?>
+    <img src="images/<?= $i ?>.png" alt="<?= $i ?>">
+    <?php
+
+}
+ 
+
+
---
+++ p2/count.php
@@ -0,0 +1,25 @@
+<?php
+$number = $_GET['number'];
+
+while ($number <= 0) {
+    
+    $number++;
+    echo $number -1  .'<br>';   // -1 to show starting value
+
+}
+?>
+<br>
+<?php
+
+ $number = $_GET['number'];
+  while ($number >=0) {
+    
+     $number--;
+     echo $number + 1 .'<br>';   // +1 to show starting value
+
+}
+    
+    
+
+
+ 
